fc18efc8f1cdf259f74b255454dd76a6436c83bd
leoul <leoul.emiru@gmail.com>
2022-12-16 12:40:51 -0500

    updated gitignore

---
+++ androidApp/build/generated/source/buildConfig/debug/comm/kmm/login/android/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package comm.kmm.login.android;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "comm.kmm.login.android";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
